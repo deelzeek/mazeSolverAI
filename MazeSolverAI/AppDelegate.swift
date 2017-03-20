73d176dd9c851582d389c3bf8287c8387f84ed37
Random Guy <deel@UsDiTo.local>
2017-03-20 17:31:51 +0100

    Initial Commit

---
+++ MazeSolverAI/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MazeSolverAI
+//
+//  Created by Deel Usmani on 20/03/2017.
+//  Copyright © 2017 Deel Usmani. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
