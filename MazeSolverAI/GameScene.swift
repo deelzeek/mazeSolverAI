83ae7017f759a040d9e2e8fe7025e0e4dbfaa1fa
Random Guy <deel@UsDiTo.local>
2017-03-20 23:21:56 +0100

    Reading a map from txt file implemented. Customized UI for Maze. Few controller are added, but functions not yet implemented.

--- MazeSolverAI/GameScene.swift
+++ MazeSolverAI/GameScene.swift
@@ -11,55 +11,62 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var tile : SKTileMapNode?
+    //private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        self.tile = self.childNode(withName: "tileMap") as? SKTileMapNode
+        
+        if let tile = self.tile {
+            let path = Bundle.main.path(forResource: "mazethree.txt", ofType: nil)
+            do {
+                let fileContents = try String(contentsOfFile:path!, encoding: String.Encoding.utf8)
+                let lines = fileContents.components(separatedBy: "\n")
+                print(lines)
+                
+                for row in 0..<lines.count {
+                    let items = lines[row].components(separatedBy: " ")
+                    
+                    for column in 0..<items.count {
+                        
+                        if items[column] != "00" {
+                            let aTile = self.tile?.tileSet.tileGroups.first(where: { $0.name == "Grass"})
+                            //let aTile = SKTileDefinition(texture: SKTexture(imageNamed: "Grass_Grid_Center"))
+                            //let tileGroup = SKTileGroup(tileDefinition: aTile)
+                            self.tile?.setTileGroup(aTile, forColumn: column, row: 7 - row)
+                            print("column: \(column), row: \(row)")
+                            
+                        }
+
+                    }
+                }
+            } catch {
+                print("Error loading map")
+            }
+
+            //label.alpha = 0.0
+            //label.run(SKAction.fadeIn(withDuration: 2.0))
         }
         
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //let w = (self.size.width + self.size.height) * 0.05
+        //self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func mouseDown(with event: NSEvent) {
@@ -76,17 +83,20 @@ class GameScene: SKScene {
     
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
+        //case 0x31:
+            /*if let tile = self.tile {
                 label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
+            }*/
         default:
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
+            print(tile?.tileSet.tileGroups.first?.name)
+           
         }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
     }
 }
